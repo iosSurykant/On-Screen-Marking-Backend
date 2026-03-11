d292647e5f0d0b362f178440c6d67b8b399a24f1
skatiyar224 <skatiyar224@gmail.com>
2026-03-11 10:10:50 +0530

    Initial Commit

---
+++ models/EvaluationModels/bookletAnswerPdfModel.js
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+
+const bookletAnswerPdfSchema = new mongoose.Schema(
+  {
+    bookletTaskId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "BookletTask",
+      required: true,
+    },
+    answerPdfName: {
+      type: String,
+      required: true,
+    },
+    status: {
+      type: String,
+      enum: ["false", "progress", "true", "reject"],
+      default: "false",
+    },
+    assignedDate: {
+      type: Date,
+      required: true,
+      index: true,
+    },
+    rejectionReason: String,
+    rejectedAt: Date,
+  },
+  { timestamps: true },
+);
+
+const BookletAnswerPdf = mongoose.model(
+  "BookletAnswerPdf",
+  bookletAnswerPdfSchema,
+);
+
+export default BookletAnswerPdf;
---
+++ models/taskModels/bookletTaskModel.js
@@ -0,0 +1,54 @@
+import mongoose from "mongoose";
+
+const bookletTaskSchema = new mongoose.Schema(
+  {
+    subjectCode: {
+      type: String,
+      required: true,
+    },
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    totalBooklets: {
+      type: Number,
+      required: true,
+    },
+    status: {
+      type: String,
+      enum: ["inactive", "active", "paused", "success"],
+      default: "inactive",
+    },
+    currentFileIndex: {
+      type: Number,
+      default: 1,
+    },
+    startTime: {
+      type: Date,
+      default: null,
+    },
+    remainingTimeInSec: {
+      type: Number,
+      default: null,
+    },
+    lastResumedAt: {
+      type: Date,
+      default: null,
+    },
+    taskType: {
+      type: String,
+      enum: ["booklet"],
+      default: "booklet",
+    },
+    efficiency: {
+      type: [Number],
+      default: [],
+    },
+  },
+  { timestamps: true },
+);
+
+const BookletTask = mongoose.model("BookletTask", bookletTaskSchema);
+
+export default BookletTask;
